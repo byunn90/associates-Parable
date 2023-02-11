e0b01a0135b56ef5e42c072ad153de0601976076
byunn90 <kayhanturk90@Live.com>
2023-02-12 05:45:40 +1100

    finshed with manager.test.js and intern.test.js have 2 more to go

---
+++ tests/Intern.test.js
@@ -0,0 +1,44 @@
+const Intern = require("../lib/Intern");
+describe("Itern", () => {
+  it("Must have the correct properties and method name", () => {
+    const myName = "Kayhan";
+    const intern = new Intern(
+      "Kayhan",
+      2255,
+      "kayhanturk90@live.com",
+      "Monash"
+    );
+    expect(intern.name).toBe(myName);
+  });
+  it("Must have the correct properties and method ID", () => {
+    const myID = 2255;
+    const intern = new Intern(
+      "Kayhan",
+      2255,
+      "kayhanturk90@live.com",
+      "Monash"
+    );
+    expect(intern.id).toBe(myID);
+  });
+  it("Must have the correct properties and method email address", () => {
+    const email = "kayhanturk90@live.com";
+    const intern = new Intern(
+      "Kayhan",
+      2255,
+      "kayhanturk90@live.com",
+      "Monash"
+    );
+    expect(intern.email).toBe(email);
+  });
+  it("Must have the correct properties and method of school", () => {
+    const school = "Monash";
+    const intern = new Intern(
+      "Kayhan",
+      2255,
+      "kayhanturk90@live.com",
+      "Monash"
+    );
+    expect(intern.school).toBe(school);
+  });
+});
+// Tested and it works
--- tests/Manager.test.js
+++ tests/Manager.test.js
@@ -1,23 +1,45 @@
 const Manager = require("../lib/Manager");
 
 describe("Manager", () => {
-  it("should have the correct properties and methods", () => {
+  it("should have the correct properties and methods NAME", () => {
+    const myName = "Kayhan";
     const manager = new Manager(
       "Kayhan",
       12345,
       "kayhanturk90@Live.com",
       98989
     );
-    expect(manager.name).toBe("Kayhan");
-    expect(manager.id).toBe(12345);
-    expect(manager.email).toBe("kayhanturk90@Live.com");
-    expect(manager.officeNumber).toBe(98989);
+    expect(manager.name).toBe(myName);
+  });
+  it("should have the correct properties and methods ID", () => {
+    const myID = 12345;
+    const manager = new Manager(
+      "Kayhan",
+      12345,
+      "kayhanturk90@Live.com",
+      98989
+    );
+    expect(manager.id).toBe(myID);
+  });
+  it("Must have the correct properties and method as email", () => {
+    const emailAdress = "kayhanturk90@Live.com";
+    const manager = new Manager(
+      "Kayhan",
+      12345,
+      "kayhanturk90@Live.com",
+      98989
+    );
+    expect(manager.email).toBe(emailAdress);
+  });
+  it("Must have the correct properties and method as email", () => {
+    const officeNumber = 98989;
+    const manager = new Manager(
+      "Kayhan",
+      12345,
+      "kayhanturk90@Live.com",
+      98989
+    );
+    expect(manager.officeNumber).toBe(officeNumber);
   });
 });
-// expect(manager.id).toBe(id);
-// expect(manager.email).toBe(email);
-// expect(manager.name).toBe(myName);
-// expect(manager.officeNumber).tobe(office);
-// expect(manager.id).toBe(12345);
-// expect(manager.email).toBe("kayhanturk90@Live.com");
-// expect(manager.officeNumber).toBe(98989);
+// Worked and tested
